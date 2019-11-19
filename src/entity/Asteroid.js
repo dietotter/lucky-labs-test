6bc23a3c81fca9fb4e7d59c9c2f82581d8917f65
Nik <nikibar4@gmail.com>
2019-11-20 00:40:28 +0200

    Bullets and asteroids are moving

--- src/entity/Asteroid.js
+++ src/entity/Asteroid.js
@@ -1,16 +1,54 @@
 import { Sprite } from 'pixi.js'
 import { CONFIG } from '../constants'
 
-const { APP_WIDTH, APP_HEIGHT, } = CONFIG
+const { APP_WIDTH } = CONFIG
 
 export class Asteroid extends Sprite {
-    constructor(texture, playerZone) {
+    constructor(texture, playerZone, stage, asteroids) {
         super(texture)
 
-        // randomly generate position and velocity
-        this.x = Math.floor(Math.random() * (APP_WIDTH - this.width))
-        this.y = Math.floor(Math.random() * (APP_HEIGHT - this.height - playerZone))
+        // reference stage and asteroid list
+        this.stage = stage
+        this.asteroids = asteroids
+        // set the right and lower boundary
+        this.rightBound = APP_WIDTH - this.width
+        this.lowerBound = playerZone - this.height
+
+        // randomly generate available position and velocity
+        this.x = Math.floor(Math.random() * this.rightBound)
+        this.y = Math.floor(Math.random() * this.lowerBound)
         this.vx = Math.random() - 0.5
         this.vy = Math.random() - 0.5
+    }
+
+    update() {
+        this.x += this.vx
+        this.y += this.vy
+
+        this.checkForBoundsCollision()
+    }
+
+    // change movement direction on hitting the boundary
+    checkForBoundsCollision() {
+
+        if (this.y <= 0 || this.y >= this.lowerBound) {
+            this.vy = -this.vy
+        }
+
+        if (this.x <= 0 || this.x >= this.rightBound) {
+            this.vx = -this.vx
+        }
+    }
+
+    destroy(stage) {
+        // remove asteroid from array to dereference it
+        this.asteroids.forEach((asteroid, i) => {
+            if (asteroid === this) {
+                this.asteroids.splice(i, 1)
+            }
+        })
+
+        // remove child from stage
+        this.stage.removeChild(this)
     }
 }
--- src/entity/Bullet.js
+++ src/entity/Bullet.js
@@ -4,17 +4,39 @@ import { CONFIG } from '../constants'
 const { BULLET_SPEED } = CONFIG
 
 export class Bullet extends Graphics {
-    constructor(x, y) {
+    constructor(x, y, stage, bullets) {
         super()
 
-        // randomly generate position and velocity
+        // reference stage and bullet list
+        this.stage = stage
+        this.bullets = bullets
+
+        // set position
         this.beginFill(0x66ff00)
-        this.drawCircle(x, y, 16)
+        this.drawCircle(0, 0, 16)
         this.endFill()
+        this.x = x
+        this.y = y
         this.vy = -BULLET_SPEED
     }
 
+    update() {
+        this.y += this.vy
+        // if bullet went off the screen, destroy it
+        if (this.y < -15) {
+            this.destroy()
+        }
+    }
+
     destroy(stage) {
-        stage.removeChild(this) // ???
+        // remove bullets from array to dereference them
+        this.bullets.forEach((bullet, i) => {
+            if (bullet === this) {
+                this.bullets.splice(i, 1)
+            }
+        })
+
+        // remove child from stage
+        this.stage.removeChild(this)
     }
 }
